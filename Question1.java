f420be57ef9c6fcc439416d3cd990b32c0225b5a
Bailey Henry <bailey.henry@daviestech.org>
2020-10-02 15:29:54 +0000

    done

--- Question1.java
+++ Question1.java
@@ -1,9 +1,16 @@
-
+import java.util.Scanner;
 
 class Question1 {
     public static void main(String[] args) {
 
-        /* Write your code here */
+      Scanner scan = new Scanner(System.in);
+
+      System.out.println("Give me two doubles");
+
+      double input1 = scan.nextDouble();
+      double input2 = scan.nextDouble();
+
+      System.out.println(input2 + " " + input1);
 
     }
 }
--- Question3.java
+++ Question3.java
@@ -1,9 +1,32 @@
-
+import java.util.Scanner;
 
 class Question3 {
     public static void main(String[] args) {
 
-        /* Write your code here */
+      Scanner scan = new Scanner(System.in);
+
+      boolean question1;
+      boolean question2;
+      boolean question3;
+
+      System.out.println("Java is a programming language, true or false?");
+
+      question1 = scan.nextBoolean();
+
+      System.out.println("Only numbers can be stored by variables in Java, true or false?");
+
+      question2 = scan.nextBoolean();
+
+      System.out.println("There are only 2 possible values which can be held by a boolean variable, true or false?");
+
+      question3 = scan.nextBoolean();
+
+      System.out.println("Question 1 - Your answer: " + question1 + ". Correct answer: true");
+
+      System.out.println("Question 2 - Your answer: " + question2 + ". Correct answer: false");
+
+      System.out.println("Question 3 - Your answer: " + question3 + ". Correct answer: true");
+
 
     }
 }
